b42ed397899c2b80d57c0a38fa8b62ecef4591d2
ukid <download_xxx@hotmail.com>
2025-01-20 10:17:42 +0700

    v7

--- app/info/page.tsx
+++ app/info/page.tsx
@@ -1,6 +1,26 @@
-const page = () => {
-  return (
-    <div>info</div>
-  )
-}
-export default page
+import { useEffect } from "react";
+import liff from "@line/liff";
+
+const Page = () => {
+  useEffect(() => {
+    const initializeLiff = async () => {
+      try {
+        await liff.init({ liffId: "2006795376-Kj0jbvX9" });
+
+        if (!liff.isLoggedIn()) {
+          liff.login();
+        } else {
+          console.log("Already logged in.");
+        }
+      } catch (err) {
+        console.error("LIFF Initialization failed", err);
+      }
+    };
+
+    initializeLiff();
+  }, []);
+
+  return <div>info</div>;
+};
+
+export default Page;
