54f62d0aacbbc8344a127caa0763a4360704732e
ukid <download_xxx@hotmail.com>
2025-02-19 23:53:05 +0700

    v6

---
+++ app/info4/[id]/page.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+async function getProfile({ params }) {
+  try {
+    const { id } = await params;
+    console.log("cccccccccccccccccc")
+    console.log(id)
+    const response = await fetch(
+      "https://testdonate.luangphorsodh.com:443/api/lineoa/profile/list?lineoa_userid=U9cd87cd0a095b3c1a062cab85dbf9701",
+      {
+        method: "GET",
+        headers: {
+          Authorization: "9613972343509313335bdc6a7fe20772c9bdd4ad",
+        },
+        cache: "no-store", // Ensures fresh data
+      }
+    );
+
+    if (!response.ok) {
+      throw new Error(`API request failed with status: ${response.status}`);
+    }
+
+    return await response.json();
+  } catch (error: any) {
+    console.error("Error fetching profile:", error);
+    return { error: error.message };
+  }
+}
+
+export default async function InfoPage() {
+  const profile = await getProfile();
+
+  return (profile);
+}
