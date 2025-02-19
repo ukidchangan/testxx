208fd58aafbdf466f2d070771a52dda3007493c3
ukid <download_xxx@hotmail.com>
2025-02-20 00:30:06 +0700

    v10

--- app/info4/[id]/page.tsx
+++ app/info4/[id]/page.tsx
@@ -54,15 +54,38 @@
 ////////////////////////
 type Params = Promise<{ id: string }>
 
+async function getProfile(id: string) {
+    try {
+      console.log("Fetching profile for user ID:", id);
+  
+      const response = await fetch(
+        `https://testdonate.luangphorsodh.com:443/api/lineoa/profile/list?lineoa_userid=${id}`,
+        {
+          method: "GET",
+          headers: {
+            Authorization: "9613972343509313335bdc6a7fe20772c9bdd4ad",
+          },
+          cache: "no-store", // Ensures fresh data
+        }
+      );
+  
+      if (!response.ok) {
+        throw new Error(`API request failed with status: ${response.status}`);
+      }
+  
+      return await response.json();
+    } catch (error: any) {
+      console.error("Error fetching profile:", error);
+      return { error: error.message };
+    }
+  }
+
 export default async function GPSFix(props: { params: Params }) {
   const params = await props.params;
   const id = params.id;
   console.log(id);
-
+  const profile  = getProfile(id);
   return (
-    <div>
-      info4
-      {id}
-    </div>
+      {profile}
   );
 }
