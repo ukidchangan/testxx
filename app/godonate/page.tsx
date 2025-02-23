0e310a5b9b8d4e3cdf4715a132027461ef476a67
ukid <download_xxx@hotmail.com>
2025-02-23 13:34:54 +0700

    v99

--- app/godonate/page.tsx
+++ app/godonate/page.tsx
@@ -1,13 +1,9 @@
-// app/create/page.tsx
 "use client"; // This is required to use client-side features like useState, useEffect, etc.
 
 import { useEffect, useState } from "react";
 import liff from "@line/liff";
 
-
-
 export default function CreatePage() {
-
   interface Category {
     id: number;
     name: string;
@@ -33,118 +29,118 @@ export default function CreatePage() {
     lineoa_display: string;
     lineoa_profile: string;
   }
+
+  const [categories, setCategories] = useState<Category[]>([]);
+  const [displayName, setDisplayName] = useState("Loading...");
+  const [userId, setUserId] = useState("Unknown");
+  const [profilePicture, setProfilePicture] = useState<string>("");
+  const [donorInfo, setDonorInfo] = useState<DonorInfo[]>([]);
+  const [previewImage, setPreviewImage] = useState<string | null>(null);
+
+  const getFormattedDate = () => {
+    const now = new Date();
+    const year = now.getFullYear();
+    const month = String(now.getMonth() + 1).padStart(2, '0'); // Ensure two digits
+    const day = String(now.getDate()).padStart(2, '0');
+    const hours = String(now.getHours()).padStart(2, '0');
+    const minutes = String(now.getMinutes()).padStart(2, '0');
   
-    const [categories, setCategories] = useState<Category[]>([]);
-    const [displayName, setDisplayName] = useState("Loading...");
-    const [userId, setUserId] = useState("Unknown");
-    const [profilePicture, setProfilePicture] = useState<string>("");
-    const [donorInfo, setDonorInfo] = useState<DonorInfo[]>([]);
-    const [previewImage, setPreviewImage] = useState<string | null>(null);
+    return `${year}-${month}-${day} ${hours}:${minutes}`;
+  };
 
-    const getFormattedDate = () => {
-        const now = new Date();
-        const year = now.getFullYear();
-        const month = String(now.getMonth() + 1).padStart(2, '0'); // Ensure two digits
-        const day = String(now.getDate()).padStart(2, '0');
-        const hours = String(now.getHours()).padStart(2, '0');
-        const minutes = String(now.getMinutes()).padStart(2, '0');
-      
-        return `${year}-${month}-${day} ${hours}:${minutes}`;
-      };
+  useEffect(() => {
+    if (userId !== "Unknown" && userId !== "") {
+      fetchDonorInfo(userId);
+    }
+  }, [userId]);
 
-      useEffect(() => {
-        if (userId !== "Unknown" && userId !== "") {
-          fetchDonorInfo(userId);
+  const fetchDonorInfo = async (userId: string) => {
+    const apiUrl = `/api/hello?userid=${userId}`;
+    try {
+      const response = await fetch(apiUrl, {
+        method: "GET",
+        mode: "no-cors",
+      });
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+
+      const data = await response.json();
+      setDonorInfo(data.data || []);
+    } catch (error) {
+      console.error("Error fetching donor info:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchCategoryInfo();
+  }, []);
+
+  const fetchCategoryInfo = async () => {
+    const apiUrl = `/api/getcategory`;
+    try {
+      const response = await fetch(apiUrl, {
+        method: "GET",
+        mode: "no-cors",
+      });
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+
+      const data = await response.json();
+      setCategories(data);
+    } catch (error) {
+      console.error("Error fetching donor info:", error);
+    }
+  };
+
+  useEffect(() => {
+    const initializeLiff = async () => {
+      try {
+        await liff.init({ liffId: "2006843844-y5kJv8l5" });
+
+        if (!liff.isLoggedIn()) {
+          liff.login();
+        } else {
+          const profile = await liff.getProfile();
+          setDisplayName(profile.displayName || "Unknown User");
+          setProfilePicture(profile.pictureUrl || "");
+          setUserId(profile.userId || "");
+          console.log("Already logged in.");
         }
-      }, [userId]);
-      const fetchDonorInfo = async (userId: string) => {
-    
-        const apiUrl = `/api/hello?userid=${userId}`;
-        try {
-          const response = await fetch(apiUrl, {
-            method: "GET",
-            mode: "no-cors",
-          });
-    
-          if (!response.ok) {
-            throw new Error(`HTTP error! Status: ${response.status}`);
-          }
-    
-          const data = await response.json();
-    
-          try{
-            setDonorInfo(data.data || []);
-          } catch (error) {}
-        } catch (error) {
-          console.error("Error fetching donor info:", error);
+      } catch (err) {
+        console.error("LIFF Initialization failed", err);
+        setDisplayName("Error loading profile");
+      }
+    };
 
-        }
-      };
-
-
-      useEffect(() => {
-        fetchCategoryInfo();
-      }, []);
-      const fetchCategoryInfo = async () => {
-        
-        const apiUrl = `/api/getcategory`;
-        try {
-          const response = await fetch(apiUrl, {
-            method: "GET",
-            mode: "no-cors",
-          });
-      
-          if (!response.ok) {
-            throw new Error(`HTTP error! Status: ${response.status}`);
-          }
-      
-          const data = await response.json();
-          setCategories(data);
-        } catch (error) {
-          console.error("Error fetching donor info:", error);
-      
-        }
-
-
-      };
-      useEffect(() => {
-        const initializeLiff = async () => {
-          try {
-            await liff.init({ liffId: "2006843844-y5kJv8l5" });
-    
-            if (!liff.isLoggedIn()) {
-              liff.login();
-            } else {
-              const profile = await liff.getProfile();
-              setDisplayName(profile.displayName || "Unknown User");
-              setProfilePicture(profile.pictureUrl || "");
-              setUserId(profile.userId || "");
-              console.log("Already logged in.");
-            }
-          } catch (err) {
-            console.error("LIFF Initialization failed", err);
-            setDisplayName("Error loading profile");
-          }
-        };
-    
-        initializeLiff();
-      }, []);
-
+    initializeLiff();
+  }, []);
 
   const [formData, setFormData] = useState({
     lineoa_userid: '',
     lineoa_profile: '',
     lineoa_displayname: '',
-    fullname: donorInfo[0]?.name || "",
+    fullname: "",
     amount: '100',
     attachment: '' as string | File,
     product_id: '',
     donate_date: getFormattedDate(),
     amulet_type: 'post',
     anumotana_type: 'lineoa',
-
     category: ''
   });
+
+  useEffect(() => {
+    if (donorInfo.length > 0) {
+      setFormData((prev) => ({
+        ...prev,
+        fullname: donorInfo[0]?.name || "",
+      }));
+    }
+  }, [donorInfo]);
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
@@ -153,6 +149,7 @@ export default function CreatePage() {
       [name]: value,
     }));
   };
+
   const handleSelectChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
     const { name, value } = e.target;
     setFormData((prev) => ({
@@ -161,21 +158,21 @@ export default function CreatePage() {
     }));
   };
 
-const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-  const file = e.target.files?.[0];
-  if (file) {
-    setFormData((prev) => ({
-      ...prev,
-      attachment: file,
-    }));
-    // Generate a preview URL
-    setPreviewImage(URL.createObjectURL(file));
-  }
-};
+  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+    if (file) {
+      setFormData((prev) => ({
+        ...prev,
+        attachment: file,
+      }));
+      // Generate a preview URL
+      setPreviewImage(URL.createObjectURL(file));
+    }
+  };
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-  
+
     // Create FormData object to send the file
     const data = new FormData();
     data.append("lineoa_userid", userId);
@@ -187,18 +184,18 @@ const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     data.append("donate_date", getFormattedDate());
     data.append("amulet_type", "post");
     data.append("anumotana_type", "lineoa");
-  
+
     // Append the file only if it's selected
     if (formData.attachment) {
       data.append("attachment", formData.attachment);
     }
-  
+
     try {
       const response = await fetch('/api/create-donate', {
         method: 'POST',
         body: data, // Send FormData instead of JSON
       });
-  
+
       if (response.ok) {
         const result = await response.json();
         if (result.success) {
@@ -215,40 +212,6 @@ const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
       alert('An error occurred while submitting the form.');
     }
   };
-  // const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-
-  //   e.preventDefault();
-  //   formData.lineoa_userid=userId;
-  //   formData.lineoa_profile=profilePicture;
-  //   formData.lineoa_displayname=displayName;
-  //   formData.donate_date= getFormattedDate();
-  //   formData.amulet_type='post';
-  //   formData.anumotana_type= 'lineoa';
-  //   try {
-  //     const response = await fetch('/api/create-donate', {
-  //       method: 'POST',
-  //       headers: {
-  //         'Content-Type': 'application/json',
-  //       },
-  //       body: JSON.stringify(formData),
-  //     });
-
-  //     if (response.ok) {
-  //       const result = await response.json(); // Parse the JSON response
-  //       if (result.success) {
-  //         alert('บริจาคเสร็จสิ้น');
-  //         window.location.href = "/getdonate";
-  //       } else {
-  //         alert(`Failed to create profile: ${result.message}`);
-  //       }
-  //     } else {
-  //       alert('Failed to create profile.');
-  //     }
-  //   } catch (error) {
-  //     console.error('Error:', error);
-  //     alert('An error occurred while submitting the form.');
-  //   }
-  // };
 
   return (
     <div
@@ -272,51 +235,49 @@ const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
           boxShadow: '0px 0px 10px #ddd',
         }}
       >
-     
         <h1 style={{ marginBottom: '0px', textAlign: 'center' }}>
-        {profilePicture && (
-        <img 
-          src={profilePicture} 
-          alt="Profile" 
-          style={{ borderRadius: "50%", width: "150px", height: "150px", marginBottom: "0px", textAlign: 'center' }} 
-        />
-      )}</h1>
-         <h1 style={{ marginBottom: '0px', textAlign: 'center' }}>   ผู้บริจาคเงินสาธุ</h1>
-         <p>{displayName}</p>
+          {profilePicture && (
+            <img 
+              src={profilePicture} 
+              alt="Profile" 
+              style={{ borderRadius: "50%", width: "150px", height: "150px", marginBottom: "0px", textAlign: 'center' }} 
+            />
+          )}
+        </h1>
+        <h1 style={{ marginBottom: '0px', textAlign: 'center' }}>ผู้บริจาคเงินสาธุ</h1>
+        <p>{displayName}</p>
         <form onSubmit={handleSubmit}>
           {/* Form Fields */}
 
           <div style={{ marginBottom: '15px' }}>
             <label style={{ display: 'block', marginBottom: '5px', fontWeight: 'bold' }}>ชื่อผู้บริจาค :</label>
             <input
-              type="้hidden"
+              type="text"
               name="fullname"
-              value={donorInfo[0]?.name || ""}
-    
+              value={formData.fullname}
+              onChange={handleChange}
               required
-              
               style={{ width: '100%', padding: '8px', borderRadius: '5px', border: '1px solid #ccc' }}
             />
-            {/* {donorInfo[0]?.name || ""} */}
           </div>
 
-      <div style={{ marginBottom: '15px' }}>
-        <label style={{ display: 'block', marginBottom: '5px', fontWeight: 'bold' }}>เลือกหมวดหมู่การบริจาค :</label>
-        <select
-          name="product_id"
-          value={formData.product_id}
-          onChange={handleSelectChange}
-          required
-          style={{ width: '100%', padding: '8px', borderRadius: '5px', border: '1px solid #ccc' }}
-        >
-          <option value="">-- กรุณาเลือกหมวดหมู่ --</option>
-          {categories.map((category) => (
-            <option key={category.id} value={category.id}>
-              {category.name}
-            </option>
-          ))}
-        </select>
-      </div>
+          <div style={{ marginBottom: '15px' }}>
+            <label style={{ display: 'block', marginBottom: '5px', fontWeight: 'bold' }}>เลือกหมวดหมู่การบริจาค :</label>
+            <select
+              name="product_id"
+              value={formData.product_id}
+              onChange={handleSelectChange}
+              required
+              style={{ width: '100%', padding: '8px', borderRadius: '5px', border: '1px solid #ccc' }}
+            >
+              <option value="">-- กรุณาเลือกหมวดหมู่ --</option>
+              {categories.map((category) => (
+                <option key={category.id} value={category.id}>
+                  {category.name}
+                </option>
+              ))}
+            </select>
+          </div>
 
           <div style={{ marginBottom: '15px' }}>
             <label style={{ display: 'block', marginBottom: '5px', fontWeight: 'bold' }}>จำนวนเงินบริจาค :</label>
@@ -339,13 +300,12 @@ const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
               style={{ width: '100%', padding: '8px', borderRadius: '5px', border: '1px solid #ccc' }}
             />
           </div>
-            {/* Display preview if an image is selected */}
-  {previewImage && (
-    <div style={{ marginTop: '10px', textAlign: 'center' }}>
-      <img src={previewImage} alt="Preview" style={{ maxWidth: '100%', borderRadius: '5px', border: '1px solid #ccc' }} />
-    </div>
-  )}
-
+          {/* Display preview if an image is selected */}
+          {previewImage && (
+            <div style={{ marginTop: '10px', textAlign: 'center' }}>
+              <img src={previewImage} alt="Preview" style={{ maxWidth: '100%', borderRadius: '5px', border: '1px solid #ccc' }} />
+            </div>
+          )}
 
           {/* Submit Button */}
           <div style={{ textAlign: 'center', marginTop: '20px' }}>
