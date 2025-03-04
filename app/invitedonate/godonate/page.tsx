fa13a5bb8d9749ca390b11b0663a4294672d4970
ukid <download_xxx@hotmail.com>
2025-03-04 18:03:25 +0700

    v162

--- app/invitedonate/godonate/page.tsx
+++ app/invitedonate/godonate/page.tsx
@@ -238,6 +238,9 @@ export default function CreatePage() {
     const amulet_type_text = amuletTypeOptions.find((option) => option.value === formData.amulet_type);
     const anumotana_type_text = anumotanaTypeOptions.find((option) => option.value === formData.anumotana_type);
     const product_text = categories.find((option) => option.name === formData.product_id);
+    alert("OK");
+    alert(formData.product_id);
+    alert(product_text);
     if (amulet_type_text) {
     localStorage.setItem('amulet_type_text', amulet_type_text.text );
     }
