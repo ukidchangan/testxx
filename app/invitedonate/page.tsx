3b1a586a71799d6a652aa978a99fd7c25331c447
ukid <download_xxx@hotmail.com>
2025-03-10 13:44:13 +0700

    v10009

--- app/invitedonate/page.tsx
+++ app/invitedonate/page.tsx
@@ -139,7 +139,7 @@ const InviteDonatePage = () => {
                     
 
                         ลงทะเบียน/แก้ไข
-                        <FontAwesomeIcon icon={faPenToSquare}  style={{ fontSize: '100px'}} />
+                        <FontAwesomeIcon icon={faPenToSquare}  style={{ fontSize: '50px', marginRight: '2px' }} />
                       </Link>
                     </button>
                   </td>
