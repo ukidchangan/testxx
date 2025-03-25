4e61d8312b91b323403bb3fea120f56c3698ae1e
kaotul <kaotul@hotmail.com>
2025-03-25 22:07:31 +0700

    cors

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   // Other configurations...
 
-  // allowedDevOrigins: ['testlineoa.luangphorsodh.com', 'lineoa.luangphorsodh.com', '*.luangphorsodh.com'],
+  allowedDevOrigins: ['testlineoa.luangphorsodh.com', 'lineoa.luangphorsodh.com', '*.luangphorsodh.com', 'localhost'],
 
 };
 
